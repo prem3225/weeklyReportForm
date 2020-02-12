b656f502d970a811946cb41dd99de27a3370ce0f
Prem kiran <premkiran3225@gmail.com>
2020-02-11 19:43:04 -0600

    Add weekly report form

---
+++ src/main/groovy/com/wrf/wrf/WrfApplication.groovy
@@ -0,0 +1,13 @@
+package com.wrf.wrf
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class WrfApplication {
+
+	static void main(String[] args) {
+		SpringApplication.run(WrfApplication, args)
+	}
+
+}
---
+++ src/test/groovy/com/wrf/wrf/WrfApplicationTests.groovy
@@ -0,0 +1,13 @@
+package com.wrf.wrf
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+//@SpringBootTest
+class WrfApplicationTests {
+
+//	@Test
+	void contextLoads() {
+	}
+
+}
